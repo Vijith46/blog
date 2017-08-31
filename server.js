0ebc0e1ff3ffae3e53739e387d807b675e141a4b
vijith <vijith46@gmail.com>
2017-08-31 20:46:38 +0100

    2nd push

--- server.js
+++ server.js
@@ -1,21 +1,70 @@
 
 
-var expr = require('express');
-var apps = expr();
+var express = require('express');
+var app = express();
 // apps.listen(5000, function(){
 //     console.log('Server is listening on port 5000 and ready to accept request :-)');
 // });
 
 var exhandle = require('express-handlebars');
 app.engine('handlebars', exhandle({ defaultLayout: 'main' }));
-apps.set('view engine', 'handlebars');
+app.set('view engine', 'handlebars');
 
 //apps.use(expr.static('public'));
 
-apps.use(express.static("public", {'extensions': ['html']}));
+app.get('/', function(req,res){
+  res.render('index');
+});
 
+app.get('/about',  function(req,res){
+  res.render('about', { title: 'ABOUT'});
+});
+
+app.get('/contact', function(req,res){
+  res.render('contact');
+});
+
+app.get('/experience', function(req,res){
+  res.render('experience');
+});
+
+app.get('/home', function(req,res){
+  res.render('home');
+});
+
+app.get('/git', function(req,res){
+  res.render('git',function(element){
+    // repos: element.name[0];
+  });
+});
+
+app.use(express.static("public", {'extensions': ['html']}));
+ 
 
 // what does this line mean: process.env.PORT || 3000
-apps.listen(process.env.PORT || 5000, function () {
+app.listen(process.env.PORT || 5000, function () {
   console.log("Server is listening on port 5000. Ready to accept requests!");
 });
+
+var api =  'https://api.github.com/users/vijith46/repos'
+var fetch = require('node-fetch');
+//var gitrepos = [];
+
+fetch(api)
+    .then(function(res) {
+        return res.json();
+    }).then(function(json) {
+      json.forEach(function(element) {
+        console.log(element.name);
+        //gitrepos = JSON.stringify(element.name);
+        //console.log(gitrepos);
+        //return gitrepos;
+      }, this);
+      console.log(json);
+    });
+
+
+
+
+
+
